c3d3969fa1eaf3b78d23eba2cab9ceeb254ce865
Al <asavicheva@internet.ru>
2026-03-18 12:58:28 +0300

    Initial Commit

---
+++ GratefulMoments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GratefulMoments
+//
+//  Created by Александра Савичева on 18.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Александра Савичева on 18.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
